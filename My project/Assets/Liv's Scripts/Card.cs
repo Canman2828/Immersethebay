d5c868632a60455b3341978123140444741ea670
Olivia Franken <89462842+ofranken@users.noreply.github.com>
2025-11-16 06:38:52 -0600

    game

---
+++ My project/Assets/Liv's Scripts/Card.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+// ========== BASE CARD CLASS ==========
+[AddComponentMenu("")]
+public abstract class Card : MonoBehaviour
+{
+    // Base class for both card types
+}
